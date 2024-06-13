5fbc20e050c4d2f42c40481813b0bde61f530575
LingYu1008 <ken89705022@gmail.com>
2024-06-13 16:01:57 +0800

    婊子們我改了速度為400

--- player.gd
+++ player.gd
@@ -4,5 +4,5 @@ extends CharacterBody2D
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right"
 	,"move_up","move_down") 
-	velocity = direction * 1600
+	velocity = direction * 400
 	move_and_slide()
